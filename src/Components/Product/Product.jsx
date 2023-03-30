68e51acc71f3c74a800516f6c35c201d9030253f
Rubel Rudra <rubelrudra27@gmail.com>
2023-03-30 16:04:27 +0600

    use array in state

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './product.css';
 
 const Product = (props) => {
     const {img,name,seller,quantity,price,ratings}=props.product;
+    const addToCart=props.buttonHandler;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -10,9 +14,11 @@ const Product = (props) => {
                 <h6 className='product-name'>{name}</h6>
                 <p>Price : ${price}</p>
                 <p className='seller'>Manufacturer : {seller}</p>
-                <p>Ratting : {ratings}</p>
+                <p>Ratting : {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={()=>addToCart(props.product)} className='btn-cart'>
+                Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
